78e4b9820462f825430a332dc7a757e3cfe336e6
Kenny Liningham <kkliningham@gmail.com>
2022-08-28 23:20:31 -0500

    SCHomeworkWeek2

---
+++ SCHomeworkWeek2.js
@@ -0,0 +1,16 @@
+"Inside your JS file, create two variables: pizzaPlace and numberOfToppings"
+"Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings."
+"PizzaPlace"
+
+"Numberoftoppings"
+
+"Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers."
+
+"Kenny's pizza place that serves 10 toppings on each pizza"
+
+"Print the variables and their types."
+
+"sausage"
+
+"Print a template literal that uses both variables to construct a short sentence about your Pizza Place."
+
